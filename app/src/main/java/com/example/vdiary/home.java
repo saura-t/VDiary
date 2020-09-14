24858d531fd5580ac4914ee0f10663c0ceb932ff
Saurabh Takle <41586938+saurabhtakle@users.noreply.github.com>
2020-09-14 19:32:58 +0530

    Homepage

--- app/src/main/java/com/example/vdiary/home.java
+++ app/src/main/java/com/example/vdiary/home.java
@@ -3,15 +3,12 @@ package com.example.vdiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button btnnewpage;
+public class home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
